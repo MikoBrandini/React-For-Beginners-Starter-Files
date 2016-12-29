f8bdff9762c451f72ff1619d2e272dab57d954fa
Miko Brandini <MikoBrandini@users.noreply.github.com>
2016-12-28 21:33:32 -0500

    completed lesson 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyCm4aA4DpVsDo2Q6j0ADJDcoOjV0nJBPVw",
+    authDomain: "catch-of-the-day-miko.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-miko.firebaseio.com",
+});
+
+export default base;
