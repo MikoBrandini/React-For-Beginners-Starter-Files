e32d3f1525c2052a00e08c0da0dc50ec6ee75bd0
Miko Brandini <MikoBrandini@users.noreply.github.com>
2016-12-29 12:20:44 -0500

    completed lesson 20

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,7 @@ constructor(){
 super();
 
 this.addFish = this.addFish.bind(this);
+this.updateFish = this.updateFish.bind(this);
 this.loadSamples = this.loadSamples.bind(this);
 this.addToOrder = this.addToOrder.bind(this);
 // initial state or known as getinitialState
@@ -57,6 +58,11 @@ addFish(fish){
               //set state
               this.setState({fishes})
 }
+updateFish(key, updatedFish){
+const fishes = {...this.state.fishes};
+fishes[key] = updatedFish;
+this.setState({ fishes });
+}
 
 loadSamples(){
   this.setState({
@@ -95,7 +101,12 @@ render(){
       order={this.state.order}
       params={this.props.params}
   />
-  <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+  <Inventory
+        addFish={this.addFish}
+        loadSamples={this.loadSamples}
+        fishes={this.state.fishes}
+        updateFish={this.updateFish}
+  />
 </div>
     )
 }
